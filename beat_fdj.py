7cffbd6578579ff864c8b384db38becaa90e932e
Imed Abid <WN5443@engie.com>
2024-03-21 13:04:34 +0100

    ML

--- beat_fdj.py
+++ beat_fdj.py
@@ -2,6 +2,9 @@ import pandas as pd
 import logging
 import utils as u
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import tensorflow as tf
 
 with open('my_log_file.log', 'w'):
     pass
@@ -25,7 +28,63 @@ df = df.dropna()
 logging.debug(f'database : {len(df)} rows')
 logging.debug(df.sample(5))
 
+# machine learning
 X = df[['BH', 'BD', 'BA']]
-Y = df['FTR']
+Y = pd.get_dummies(df['FTR'])
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.0005, random_state=42)
+
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(3,)),  # Couche d'entrée avec 3 nœuds
+    Dense(64, activation='relu'),  # Couche cachée avec 64 nœuds et fonction d'activation relu
+    Dense(3, activation='softmax')  # Couche de sortie avec 3 nœuds et fonction d'activation softmax
+])
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model.compile(
+    loss=tf.keras.losses.CategoricalCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    metrics=['accuracy']
+)
+
+history = model.fit(X_train, Y_train, epochs=50, validation_data=(X_test, Y_test))
+predictions = model.predict(X_test)
+
+Y_test_category = Y_test.idxmax(axis=1)
+predicted_categories = pd.Series(predictions.argmax(axis=1), index = X_test.index).map({0: 'H', 1: 'D', 2: 'A'})
+
+results_df = pd.DataFrame(X_test, columns=['BH', 'BD', 'BA'])
+results_df['Div'] = df.loc[X_test.index, 'Div']
+results_df['Date'] = df.loc[X_test.index, 'Date']
+results_df['HomeTeam'] = df.loc[X_test.index, 'HomeTeam']
+results_df['AwayTeam'] = df.loc[X_test.index, 'AwayTeam']
+results_df['FTR'] = Y_test_category  # Ajout des étiquettes réelles
+results_df['Predictions'] = predicted_categories  # Ajout des prédictions
+
+import matplotlib.pyplot as plt
+
+logging.debug(results_df)
+#put dataframe into excel file
+results_df.to_excel('results.xlsx')
+
+total_rows = len(results_df)
+num_correct_predictions = len(results_df[(results_df['FTR'] == results_df['Predictions']) & (results_df['FTR'] != 'Div')])
+
+print(f'Accuracy: {num_correct_predictions / total_rows * 100}%')
+
+# Affichage de la perte (loss) pendant l'entraînement
+plt.plot(history.history['loss'], label='Loss')
+plt.title('Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Affichage de la précision (accuracy) pendant l'entraînement
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.title('Training Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()  
+plt.show()
